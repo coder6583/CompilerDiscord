e9e542f644a3b016d6877fa775a31521bfd88549
curvingchicken <thedude6583@gmail.com>
2021-03-25 15:18:35 +0900

    test

---
+++ server.js
@@ -0,0 +1,18 @@
+"use strict";
+var Discord = require('discord.js');
+var dotenv = require('dotenv');
+var _a = require('./config.json'), prefix = _a.prefix, token = _a.token;
+var client = new Discord.Client();
+dotenv.config();
+client.on('ready', function () {
+    console.log("Logged in as " + client.user.tag + "!");
+});
+client.on('message', function (msg) {
+    //   if (msg.content === 'ping') {
+    //     msg.reply('Pong!');
+    //   }
+    if (msg.content == prefix + "ping") {
+        msg.channel.send('yea');
+    }
+});
+client.login(process.env.TOKEN);
--- server.ts
+++ server.ts
@@ -1,14 +1,22 @@
 const Discord = require('discord.js');
+const dotenv = require('dotenv');
+const {prefix, command, token} = require('./config.json');
+
 const client = new Discord.Client();
+dotenv.config();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+//   if (msg.content === 'ping') {
+//     msg.reply('Pong!');
+//   }
+  if(msg.content ==`${command}ping`)
+  {
+      msg.channel.send('yea');
   }
 });
 
-client.login('token');
+client.login(process.env.TOKEN);
