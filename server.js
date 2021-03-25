c023a73af79e7a7dba8a8a3f513bc6c72c0686bc
curvingchicken <thedude6583@gmail.com>
2021-03-25 18:52:46 +0900

    test

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ fs.watchFile(__dirname + '/log', function (curr, prev) {
             var change_1 = data.slice(fileSize);
             console.log(change_1.toString());
             client.channels.fetch('824546860655837194').then(function (channel) {
-                channel.send('```' + change_1.toString() + '```');
+                channel.send('``' + change_1.toString() + '`');
             });
             fileSize = data.length;
         }
